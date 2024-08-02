8eee37619535832e2e39bd35420df1485dec27a1
10138103 <jk.choi@kt.com>
2024-08-02 15:41:39 +0900

    add langgraph section 2. reflection agent

--- examples/service/ollama.py
+++ examples/service/ollama.py
@@ -24,6 +24,6 @@ if __name__ == "__main__":
 
     async def main():
         async for chunk in langchain_stream():
-            print(chunk, end='|')
+            print(chunk, end="|")
 
     asyncio.run(main())
